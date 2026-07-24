89230d88b47846a1bf1b2dbc206d8772f2af6583
Sujit <sujitprabhu1912@gmail.com>
2026-07-24 21:16:45 +0530

    Create 3(a).java

---
+++ 3(a).java
@@ -0,0 +1,43 @@
+interface Shape {
+    double calculateArea();
+}
+class Circle implements Shape {
+    double radius;
+    Circle(double radius) {
+        this.radius = radius;
+    }
+    public double calculateArea() {
+        return 3.14 * radius * radius;
+    }
+}
+class Rectangle implements Shape {
+    double length, width;
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+    public double calculateArea() {
+        return length * width;
+    }
+}
+class Triangle implements Shape {
+    double base, height;
+    Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+}
+public class ShapeAreaCalculator {
+    public static void main(String[] args) {
+        Circle c = new Circle(5);
+        Rectangle r = new Rectangle(4, 6);
+        Triangle t = new Triangle(3, 8);
+        System.out.println("----- Shape Area Calculator -----");
+        System.out.println("Circle Area = " + c.calculateArea());
+        System.out.println("Rectangle Area = " + r.calculateArea());
+        System.out.println("Triangle Area = " + t.calculateArea());
+    }
+}
