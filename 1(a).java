9f60fc59f248be28801e1cd3d2d5bbc449cdfc27
Sujit <sujitprabhu1912@gmail.com>
2026-07-24 20:35:36 +0530

    Create 1(a).java

---
+++ 1(a).java
@@ -0,0 +1,13 @@
+class ParkingSystem {  
+    private int[] slots;
+    public ParkingSystem(int big, int medium, int small) {
+        this.slots = new int[]{0, big, medium, small};
+    }    
+    public boolean addCar(int carType) {
+        if (slots[carType] > 0) {	
+            slots[carType]--;
+            return true;
+        }
+        return false;
+    }
+}
