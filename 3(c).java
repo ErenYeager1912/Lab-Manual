e7769b28ce0c22de60f946dd4c2dd1ee677356da
Sujit <sujitprabhu1912@gmail.com>
2026-07-24 20:57:58 +0530

    Create 3(c).java

---
+++ 3(c).java
@@ -0,0 +1,17 @@
+class Solution {
+public boolean checkStraightLine(int[][] coordinates) {
+        if (coordinates.length <= 2) {
+            return true;
+        }
+        int deltaX = coordinates[1][0] - coordinates[0][0];
+        int deltaY = coordinates[1][1] - coordinates[0][1];
+        for (int i = 2; i < coordinates.length; i++) {
+            int currentDeltaX = coordinates[i][0] - coordinates[0][0];
+            int currentDeltaY = coordinates[i][1] - coordinates[0][1];
+            if (deltaY * currentDeltaX != deltaX * currentDeltaY) {
+                return false;
+            }      
+         }
+        return true;
+    }
+}
